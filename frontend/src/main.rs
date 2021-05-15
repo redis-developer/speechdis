98b9b5ad7897df8deb6b291d1ee8b0fcc79442cf
Thomaub <github.thomaub@gmail.com>
2021-05-15 18:36:33 +0200

    🚧 building frontend with some models in redisAI

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,226 +1,147 @@
-#![recursion_limit = "1024"]
+// requires the serde and anyhow crates
 
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+use serde::Deserialize;
+use yew::services::fetch::{FetchService, FetchTask, Request, Response};
+use yew::{
+    format::{Json, Nothing},
+    prelude::*,
+};
 
-use console_error_panic_hook::set_once as set_panic_hook;
+// #[derive(Deserialize, Debug, Clone)]
+// pub struct ISSPosition {
+//     latitude: String,
+//     longitude: String,
+// }
 
-use ybc::TileCtx::{Ancestor, Child, Parent};
-use yew::services::reader::{File, FileChunk, FileData, ReaderService, ReaderTask};
-use yew::services::ConsoleService;
-use yew::{html, ChangeData, Component, ComponentLink, Html, ShouldRender};
+// #[derive(Deserialize, Debug, Clone)]
+// pub struct ISS {
+//     message: String,
+//     timestamp: i32,
+//     iss_position: ISSPosition,
+// }
+use shared::{ApiAIModel, ModelsApiResponse};
 
-type Chunks = bool;
-
+#[derive(Debug)]
 pub enum Msg {
-    Loaded(FileData),
-    Chunk(Option<FileChunk>),
-    Files(Vec<File>, Chunks),
-    ToggleByChunks,
+    GetListModels,
+    ReceiveResponse(Result<ModelsApiResponse, anyhow::Error>),
 }
 
-pub struct Model {
-    link: ComponentLink<Model>,
-    tasks: Vec<ReaderTask>,
-    files: Vec<String>,
-    by_chunks: bool,
+#[derive(Debug)]
+pub struct FetchServiceExample {
+    fetch_task: Option<FetchTask>,
+    models: Option<ModelsApiResponse>,
+    link: ComponentLink<Self>,
+    error: Option<String>,
+}
+/// Some of the code to render the UI is split out into smaller functions here to make the code
+/// cleaner and show some useful design patterns.
+impl FetchServiceExample {
+    fn view_iss_location(&self) -> Html {
+        match self.models {
+            Some(ref model_list) => {
+                html! {
+                    <>
+                        <p>{ "The List of available model is:" }</p>
+                        // <p>{ format!("Latitude: {}", model_list.data.iter().next().unwrap().model_name) }</p>
+                        <ul>{ for model_list.data.iter().map(|f| Self::view_file(f)) }</ul>
+                    </>
+                }
+            }
+            None => {
+                html! {
+                     <button onclick=self.link.callback(|_| Msg::GetListModels)>
+                         { "What model are available?" }
+                     </button>
+                }
+            }
+        }
+    }
+    fn view_file(data: &ApiAIModel) -> Html {
+        html! {
+            <li>{ format!("Latitude: {:?}",data) }</li>
+        }
+    }
+    fn view_fetching(&self) -> Html {
+        if self.fetch_task.is_some() {
+            html! { <p>{ "Fetching data..." }</p> }
+        } else {
+            html! { <p></p> }
+        }
+    }
+    fn view_error(&self) -> Html {
+        if let Some(ref error) = self.error {
+            html! { <p>{ error.clone() }</p> }
+        } else {
+            html! {}
+        }
+    }
 }
 
-impl Component for Model {
+impl Component for FetchServiceExample {
     type Message = Msg;
     type Properties = ();
 
     fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
         Self {
+            fetch_task: None,
+            models: None,
             link,
-            tasks: vec![],
-            files: vec![],
-            by_chunks: false,
+            error: None,
         }
     }
-
-    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+    fn change(&mut self, _props: Self::Properties) -> bool {
+        false
+    }
+    fn update(&mut self, msg: Self::Message) -> bool {
         match msg {
-            Msg::Loaded(file) => {
-                let info = format!("file: {:?}", file);
-                self.files.push(info);
+            Msg::GetListModels => {
+                // 1. build the request
+                let request = Request::get("http://127.0.0.1:8000/api/v1/admin/models/list")
+                    .body(Nothing)
+                    .expect("Could not build request.");
+                // 2. construct a callback
+                let callback = self.link.callback(
+                    |response: Response<Json<Result<ModelsApiResponse, anyhow::Error>>>| {
+                        let Json(data) = response.into_body();
+                        Msg::ReceiveResponse(data)
+                    },
+                );
+                // 3. pass the request and callback to the fetch service
+                let task = FetchService::fetch(request, callback).expect("failed to start request");
+                // 4. store the task so it isn't canceled immediately
+                self.fetch_task = Some(task);
+                // we want to redraw so that the page displays a 'fetching...' message to the user
+                // so return 'true'
                 true
             }
-            Msg::Chunk(Some(chunk)) => {
-                let info = format!("chunk: {:?}", chunk);
-                self.files.push(info);
-                true
-            }
-            Msg::Files(files, chunks) => {
-                let mut reader_service = ReaderService::new();
-                for file in files.into_iter() {
-                    let task = {
-                        if chunks {
-                            let callback = self.link.callback(Msg::Chunk);
-                            reader_service
-                                .read_file_by_chunks(file, callback, 100)
-                                .unwrap()
-                        } else {
-                            let callback = self.link.callback(Msg::Loaded);
-                            reader_service.read_file(file, callback).unwrap()
-                        }
-                    };
-                    self.tasks.push(task);
+            Msg::ReceiveResponse(response) => {
+                match response {
+                    Ok(location) => {
+                        self.models = Some(location);
+                    }
+                    Err(error) => self.error = Some(error.to_string()),
                 }
+                self.fetch_task = None;
+                // we want to redraw so that the page displays the location of the ISS instead of
+                // 'fetching...'
                 true
             }
-            Msg::ToggleByChunks => {
-                self.by_chunks = !self.by_chunks;
-                true
-            }
-            _ => false,
         }
     }
-
-    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-        false
-    }
-
-    fn view(&self) -> Html {
-        let flag = self.by_chunks;
-        html! {
-            <div>
-                <div>
-                    <h1>{ "Choose a file to upload to see the uploaded bytes" }</h1>
-                    <input type="file" multiple=true onchange=self.link.callback(move |value| {
-                            let mut result = Vec::new();
-                            if let ChangeData::Files(files) = value {
-                                let files = js_sys::try_iter(&files)
-                                    .unwrap()
-                                    .unwrap()
-                                    .map(|v| File::from(v.unwrap()));
-                                result.extend(files);
-                            }
-                            Msg::Files(result, flag)
-                        })
-                    />
-                </div>
-                <div>
-                    <label>{ "By chunks" }</label>
-                    <input type="checkbox" checked=flag onclick=self.link.callback(|_| Msg::ToggleByChunks) />
-                </div>
-                <ul>
-                    { for self.files.iter().map(|f| Self::view_file(f)) }
-                </ul>
-            </div>
-        }
-    }
-}
-
-impl Model {
-    fn view_file(data: &str) -> Html {
-        html! {
-            <li>{ data }</li>
-        }
-    }
-}
-struct App;
-
-impl Component for App {
-    type Message = ();
-    type Properties = ();
-
-    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        Self
-    }
-
-    fn update(&mut self, _: Self::Message) -> bool {
-        false
-    }
-
-    fn change(&mut self, _: Self::Properties) -> bool {
-        false
-    }
-
     fn view(&self) -> Html {
         html! {
             <>
-            <ybc::Navbar
-                classes="is-info"
-                padded=true
-                navbrand=html!{
-                    <ybc::NavbarItem>
-                        <ybc::Title classes="has-text-white" size=ybc::HeaderSize::Is4>{"Trunk | Yew | YBC"}</ybc::Title>
-                    </ybc::NavbarItem>
-                }
-                navstart=html!{}
-                navend=html!{
-                    <>
-                    <ybc::NavbarItem>
-                        <ybc::ButtonAnchor classes="is-black is-outlined" rel="noopener noreferrer" target="_blank" href="https://github.com/thedodd/trunk">
-                            {"Trunk"}
-                        </ybc::ButtonAnchor>
-                    </ybc::NavbarItem>
-                    <ybc::NavbarItem>
-                        <ybc::ButtonAnchor classes="is-black is-outlined" rel="noopener noreferrer" target="_blank" href="https://yew.rs">
-                            {"Yew"}
-                        </ybc::ButtonAnchor>
-                    </ybc::NavbarItem>
-                    <ybc::NavbarItem>
-                        <ybc::ButtonAnchor classes="is-black is-outlined" rel="noopener noreferrer" target="_blank" href="https://github.com/thedodd/ybc">
-                            {"YBC"}
-                        </ybc::ButtonAnchor>
-                    </ybc::NavbarItem>
-                    </>
-                }
-            />
-
-            <ybc::Hero
-                classes="is-light"
-                size=ybc::HeroSize::FullheightWithNavbar
-                body=html!{
-                    <ybc::Container classes="is-centered">
-                    <ybc::Tile ctx=Ancestor>
-                        <ybc::Tile ctx=Parent size=ybc::TileSize::Twelve>
-                            <ybc::Tile ctx=Parent>
-                                <ybc::Tile ctx=Child classes="notification is-success">
-                                    <ybc::Subtitle size=ybc::HeaderSize::Is3 classes="has-text-white">{"Trunk"}</ybc::Subtitle>
-                                    <p>{"Trunk is a WASM web application bundler for Rust."}</p>
-                                </ybc::Tile>
-                            </ybc::Tile>
-                            <ybc::Tile ctx=Parent>
-                                <ybc::Tile ctx=Child classes="notification is-success">
-                                    <ybc::Icon size=ybc::Size::Large classes="is-pulled-right"><img src="yew.svg"/></ybc::Icon>
-                                    <ybc::Subtitle size=ybc::HeaderSize::Is3 classes="has-text-white">
-                                        {"Yew"}
-                                    </ybc::Subtitle>
-                                    <p>{"Yew is a modern Rust framework for creating multi-threaded front-end web apps with WebAssembly."}</p>
-                                </ybc::Tile>
-                            </ybc::Tile>
-                            <ybc::Tile ctx=Parent>
-                                <ybc::Tile ctx=Child classes="notification is-success">
-                                    <Model> </Model>
-                                    // <ybc::Subtitle size=ybc::HeaderSize::Is3 classes="has-text-white">{"YBC"}</ybc::Subtitle>
-                                    // <p>{"A Yew component library based on the Bulma CSS framework."}</p>
-                                </ybc::Tile>
-                            </ybc::Tile>
-                        </ybc::Tile>
-                    </ybc::Tile>
-                    </ybc::Container>
-                }>
-            </ybc::Hero>
+                { self.view_fetching() }
+                { self.view_iss_location() }
+                { self.view_error() }
             </>
         }
     }
 }
 
 fn main() {
-    set_panic_hook();
-    ConsoleService::log("hello");
-    // Show off some feature flag enabling patterns.
-    #[cfg(feature = "demo-abc")]
-    {
-        ConsoleService::log("feature `demo-abc` enabled");
-    }
-    #[cfg(feature = "demo-xyz")]
-    {
-        ConsoleService::log("feature `demo-xyz` enabled");
-    }
-
-    yew::start_app::<App>();
+    // set_panic_hook();
+    // ConsoleService::log("hello");
+    yew::start_app::<FetchServiceExample>();
 }
